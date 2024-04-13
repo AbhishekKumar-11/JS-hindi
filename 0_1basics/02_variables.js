a1a0a78394107c8931ef43ab33ceaa1ea0d45877
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-13 20:07:36 +0000

    data types

---
+++ 0_1basics/02_variables.js
@@ -0,0 +1,21 @@
+"use strict" // yah a seh moderen JS keh jaisa treat karega 
+
+// alert(3+3) // we are using node so dont use it
+
+console.log(3+
+3); // code reedeability
+
+/* data types */
+
+// number 89
+// string "yhj"
+// null => stand alone
+// booleam true/false 
+/// undefined => 
+// symbol => unique  // used more in react 
+
+/// objects 
+
+console.log(typeof null); // objects 
+console.log(typeof undefined) ; // undefined 
+ 
