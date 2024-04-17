a68c8d8037448025cd64e0f5d806ff4804f675f7
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-17 02:55:02 +0000

    Conversion of dataTypes

---
+++ 0_1basics/03_typeConversion.js
@@ -0,0 +1,29 @@
+ let score = "33abc" ;
+
+ 
+ let valueInNumber = Number(score);
+// console.log(valueInNumber);  // NaN
+// console.log(typeof valueInNumber); // type of NaN is number so it's log is  number but actually the value coverted id NaN 
+// null => 0  ; type of Object 
+// undefined  =>NaN ; typeof undefined 
+   // "33" => 33 
+   // "33abc" => NaN
+   // true =  1 ; false = 0 ; 
+
+
+   let isLoggedIn = 0
+
+   let booleanIsLoggedIn = Boolean(isLoggedIn)
+       console.log(typeof booleanIsLoggedIn);
+      console.log(booleanIsLoggedIn);
+     
+// 
+ //  1 =>  true   ; 0  => false  ;
+// "abhishek kumar" => true ; "" => false    // "0" => true  
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
