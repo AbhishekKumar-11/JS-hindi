5d6065c38f217d98b3401358ed7e7a805116d0fc
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-17 03:25:23 +0000

    operations added

--- 0_1basics/03_typeConversion.js
+++ 0_1basics/03_typeConversion.js
@@ -14,8 +14,8 @@
    let isLoggedIn = 0
 
    let booleanIsLoggedIn = Boolean(isLoggedIn)
-       console.log(typeof booleanIsLoggedIn);
-      console.log(booleanIsLoggedIn);
+    //    console.log(typeof booleanIsLoggedIn);
+    //   console.log(booleanIsLoggedIn);
      
 // 
  //  1 =>  true   ; 0  => false  ;
@@ -27,3 +27,50 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+// ******************************************Operastors *****************************************************************
+
+
+// console.log(2+3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2-3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Abhishek kumar "
+
+let str3 = str1 + str2 
+
+// console.log( 1 +  "2");  12
+// console.log( "1" +  2);   12
+// console.log( "1" +  2 + 2 );  122 
+// console.log( 1 +  2 + "2" );  32 
+// console.log( "1" +  "2");  12 
+
+// console.log( (3 + 4) * 5 % 3); this is not recommeded if anuy type of operation preffer parenthesis
+
+  //  console.log(+true);  // 1  
+ //     console.log(+""); // 0
+
+ 
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // this not recommdede yto write like this  
+
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);  // 101
+
+ // here  in this two cant see any diffrence but there is a diffrence b/w prefix and postfix increment and decrement 
+ // as prefix first increment/ decrement then use or assign 
+ // as postfix first use or perform operation and then increment / decrement 
+
+
+let GameCounter = 100
+GameCounter++;
+console.log(GameCounter); // 101 
