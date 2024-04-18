c802283e331183ddf2573aa70d8546d2b2cb6768
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-18 20:15:57 +0000

    stack and heap  memory

--- 01_basics/dataypeSummary.js
+++ 01_basics/dataypeSummary.js
@@ -36,4 +36,34 @@ const myFunction = function(){
 }
 
 
- 
+ // ++++++++++++++++++++++++++++++++++++++++++++++++Stack and Heap memory++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+
+
+//    primitives(Stack) and reffrences (Heap)
+
+   
+   // as in primitive  the copy of the  datatypes is given as hte changers made in one doesn't reflect each other 
+   let a = 75 ;
+
+   let b = a;  
+   b=5;
+   console.log(a); // 75   
+   console.log(b);  //5
+
+// in  reffrences or non-pritive the  datatypes are passed by reffrences (means that the diffrent variable
+// reffer to same adddress  ) as the  changes done will reflect to another because they both point to same location or address
+
+ let userOne = {
+    name : "abhishek@gmail.com",
+    id :"2141011160"
+ }
+
+ let userTwo = userOne;
+
+ userTwo.name = "rishu@2152@gmial.com";
+
+ console.log(userOne.name); // rishu@2152@gmial.com
+ console.log(userTwo.name);  // rishu@2152@gmial.com
+
+
+
