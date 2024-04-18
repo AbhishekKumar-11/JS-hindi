7b65cdf03d1dc9d5e91afa141af385c2db4e481d
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-18 18:03:29 +0000

    rename

---
+++ 0_1basics/01variables.js
@@ -0,0 +1,7 @@
+let suwar = "harshit ranjan";
+const Admi = "Kaushik_anand";
+var moddy = "Mrinalbhiya";
+
+/*
+var nahi use karna kyo ki block scope kah dikkat keh wajah seh aur functional scope
+*/
