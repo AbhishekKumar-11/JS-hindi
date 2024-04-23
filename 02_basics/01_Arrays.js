7a3630ca07af08e7b7bc7646b854d839112d1ae2
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-23 11:54:34 +0000

    coorected the indentaion

--- 02_basics/01_Arrays.js
+++ 02_basics/01_Arrays.js
@@ -1,36 +1,33 @@
-// arrays 
+// arrays
 
-let myArray =[0,1,2,3,4,5] ;
-// Array Methods 
+let myArray = [0, 1, 2, 3, 4, 5];
+// Array Methods
 
-// console.log(myArray.push(10));  // retuns the new length of the array and push the elememnts to the last of the array 
+// console.log(myArray.push(10));  // retuns the new length of the array and push the elememnts to the last of the array
 
-// console.log(myArray.pop()); // pop the element from the last of the array and return the elements which is poped 
+// console.log(myArray.pop()); // pop the element from the last of the array and return the elements which is poped
 
-// console.log(myArray.unshift(30)); // add the elements in the starting of the array  and return the new length aftere insertion of thr new element 
+// console.log(myArray.unshift(30)); // add the elements in the starting of the array  and return the new length aftere insertion of thr new element
 
+// console.log(myArray.shift()); // removes the element  from the begning of the array and return th element which is removed
 
+// it alaso includes all the methods towards  that  use to chech off
 
-// console.log(myArray.shift()); // removes the element  from the begning of the array and return th element which is removed 
-
-
-// it alaso includes all the methods towards  that  use to chech off 
-
-// //  console.log( myArray.includes(5)); //checks  if thast particular  presentr in  the array or not 
-// // console.log(myArray.indexOf(4));  // returns the index of the given element if the element is in the array 
+// //  console.log( myArray.includes(5)); //checks  if thast particular  presentr in  the array or not
+// // console.log(myArray.indexOf(4));  // returns the index of the given element if the element is in the array
 
 // let newArray = myArray.join();
 
-// console.log(newArray);// reutns the array in form of string 
-// console.log(typeof newArray); //String 
+// console.log(newArray);// reutns the array in form of string
+// console.log(typeof newArray); //String
 
-// splice and slice // the differnce between is that the slice method does not modify the array while splice modify the array 
+// splice and slice // the differnce between is that the slice method does not modify the array while splice modify the array
 
-console.log( "A " ,  myArray);
-console.log(myArray.slice(1,2));
-console.log( "B",myArray); //the array is not modified as the element at index 1  as sliced out from array and the last one is not inclued
-// 
+console.log("A ", myArray);
+console.log(myArray.slice(1, 2));
+console.log("B", myArray); //the array is not modified as the element at index 1  as sliced out from array and the last one is not inclued
+//
 
-console.log( "the element which are spliced out -> ",myArray.splice(1,2));
-// we can observe that the  original array has been modified 
-console.log( "C " ,myArray);
+console.log("the element which are spliced out -> ", myArray.splice(1, 2));
+// we can observe that the  original array has been modified
+console.log("C ", myArray);
--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -1,28 +1,28 @@
-
 // function addTwoNumbers(number1 , number2) {
 //     console.log(number1 + number2);
-// } 
- //the function doesn't return anything as num will be undefined 
- const num = addTwoNumbers(5,3);
+// }
+//the function doesn't return anything as num will be undefined
+const num = addTwoNumbers(5, 3);
 //console.log(num);
 
-
-function addTwoNumbers(number1 , number2) { // parametrs are those which we define at the time definig the function 
-     return   number1 + number2 ;
-    } 
-    // here the functions returns so num1 will not be undefined 
-const num1 = addTwoNumbers(5,10);// arguments are those which we pass at call the function or we can say the real value that re p
+function addTwoNumbers(number1, number2) {
+  // parametrs are those which we define at the time definig the function
+  return number1 + number2;
+}
+// here the functions returns so num1 will not be undefined
+const num1 = addTwoNumbers(5, 10); // arguments are those which we pass at call the function or we can say the real value that re p
 // passed during the call of function is called Arguments .
 //console.log(num1);
 
- function loginUserMessage(userName="sam"){ //  we can also give default value to the parameters
-         if(!userName){
-            console.log("please enter the username ");
-            return ;
-         }
-    console.log(`${userName} logged in now `);
- }
+function loginUserMessage(userName = "sam") {
+  //  we can also give default value to the parameters
+  if (!userName) {
+    console.log("please enter the username ");
+    return;
+  }
+  console.log(`${userName} logged in now `);
+}
 // if the arguments are empty then it logs // undefined logged in now
-  loginUserMessage();
+loginUserMessage();
 
- loginUserMessage("abhishek kumar" );
+loginUserMessage("abhishek kumar");
