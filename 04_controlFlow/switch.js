cda73069c6846db4b419623b792e9b82b94b9115
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-27 19:58:35 +0000

    switch

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,32 @@
+
+
+switch (key) {  // syntax 
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+// const month = "march" // whatever is the keyword that is being put in the case 
+
+// switch (month) {
+//     case "jan":
+//         console.log("January");
+//         break;  // break keyword is important otherwise the conditon matched afterwards all conditions will be executed 
+//                   default line segment (Many times asked in interviews)
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;  // default also has break ; 
+// }
