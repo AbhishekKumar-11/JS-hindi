2de78ee1a379c0ddead4ec2be88511d900798a1a
AbhishekKumar-11 <126537237+AbhishekKumar-11@users.noreply.github.com>
2024-04-28 10:59:40 +0000

    while , do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// while loop 
+
+let i =0 ;
+ while (i <= 10){
+    console.log(i);
+    i += 2 ;
+ }
+
+ let myArray = ['flash', "batman", "superman"] 
+  let arr =0 ;
+  while(arr < myArray.length){
+    console.log(myArray[arr]);
+    arr++;
+  }
+
+
+   // do while loop first excecute the do block first time  then it chechs the while condition  and moves further if 
+   // let score =0 ;
+    let score = 11 ;
+   
+   do{
+   console.log(score);
+   score++
+   }
+   while(score <= 10) ; 
